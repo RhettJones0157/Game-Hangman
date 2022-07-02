8b508be4ce91691c632ae157db3b7cb86a27ded7
Rhettj0157 <79929919+RhettJones0157@users.noreply.github.com>
2022-07-02 05:00:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import random
+tries = 5
+trydown == False
+wordlibrary = ['speech', 'crazy', 'physique']
+wordlistlen = len(wordlibrary)
+word = wordlibrary[random.randint(0, wordlistlen - 1)]
+word = list(word)
+
+hangmanword = list[]
+for i in range(wordlistlen):
+  hangmanword.append('_')
+
+userletter = input("Enter a letter: ")
+while tries > 0
+  for i in word:
+    if i == userletter:
+      #get index value of userletter in word
+      #substitue hangmanword for index value of userletter. print
+    trydown == True
+  if 
+  
+    
